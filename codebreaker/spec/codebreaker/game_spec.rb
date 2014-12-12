315ce0fd7525c0921b2fc65b7034808718a1c344
cema-sp <cema.rus@gmail.com>
2014-12-12 16:28:04 +0300

    Starting 6th chapter

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
+    let(:output_stream) { double('output_stream').as_null_object }
+    let(:game) { Game.new(output_stream) }
+
     describe '#start' do
-      let(:output_stream) { double('output_stream').as_null_object }
-      let(:game) { Game.new(output_stream) }
 
       it 'sends a welcome message' do
         expect(output_stream).to receive(:puts).with('Welcome to Codebreaker!')
@@ -14,6 +15,24 @@ module Codebreaker
       it 'prompts for the first guess' do
         expect(output_stream).to receive(:puts).with('Enter guess:')
         game.start('1234')
+      end
+    end
+
+    describe '#guess' do
+      context 'with no matches' do
+        it 'sends a mark with \'\'' do
+          game.start('1234')
+          expect(output_stream).to receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+
+      context 'with 1 number match' do
+        it 'sends a mark with \'-\'' do
+          game.start('1234')
+          expect(output_stream).to receive(:puts).with('-')
+          game.guess('2555')
+        end
       end
     end
   end
